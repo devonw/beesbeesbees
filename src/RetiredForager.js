a6ac5e5aea3aaced24feac1237ccef47a9966f8f
Devon Weldon <deveathbo@gmail.com>
2017-03-27 11:41:05 -0700

    Finished all of the beesbeesbees assignment

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,24 @@
 var RetiredForagerBee = function() {
+  Grub.call(this);
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble'
+  this.canFly = false;
+  this.color = 'grey';
+  
 };
+
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+
+RetiredForagerBee.prototype.gamble = function() {
+  this.treasureChest.push(1);
+}
+
+RetiredForagerBee.prototype.forage = function() {
+  	return 'I am too old, let me play cards instead';
+  };
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+
 
